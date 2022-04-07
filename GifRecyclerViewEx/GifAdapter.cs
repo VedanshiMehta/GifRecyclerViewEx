155fe5edac0f58c287da6d6450f52f9f1d14e985
vedanshi.mehta <vedanshi.mehta@1R-76>
2022-04-07 15:38:51 +0530

    Add project files.

---
+++ GifRecyclerViewEx/GifAdapter.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.RecyclerView.Widget;
+using BumpTech.GlideLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GifRecyclerViewEx
+{
+    class GifAdapter : RecyclerView.Adapter
+    {
+        private List<int> gifs;
+        private MainActivity mainActivity;
+
+        public GifAdapter(List<int> gifs, MainActivity mainActivity)
+        {
+            this.gifs = gifs;
+            this.mainActivity = mainActivity;
+        }
+
+        public override int ItemCount => gifs.Count;
+
+        public override void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            GifViewHolder gifViewHolder = holder as GifViewHolder;
+            Glide.With(mainActivity).Load(gifs[position]).Into(gifViewHolder._imageView);
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View view = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.gifListLayout, parent, false);
+            return new GifViewHolder(view);
+        }
+
+        class GifViewHolder : RecyclerView.ViewHolder
+        {
+            public ImageView _imageView;
+            public GifViewHolder(View itemView) : base(itemView)
+            {
+                _imageView = itemView.FindViewById<ImageView>(Resource.Id.imageViewGif);
+            }
+        }
+    }
+}
---
+++ GifRecyclerViewEx/MainActivity.cs
@@ -0,0 +1,72 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using AndroidX.RecyclerView.Widget;
+using BumpTech.GlideLib;
+using System;
+using System.Collections.Generic;
+
+namespace GifRecyclerViewEx
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private RecyclerView _recyclerView;
+        private RecyclerView.LayoutManager _layoutManager;
+        private GifAdapter _gifAdapter;
+        private List<int> _gifs;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            //_imageView = FindViewById<ImageView>(Resource.Id.imageViewGif);
+            //Glide.With(this).Load(Resource.Drawable.minion).Into(_imageView);
+
+            _recyclerView = FindViewById<RecyclerView>(Resource.Id.recyclerViewGif);
+
+            GetGifs();
+            _layoutManager = new GridLayoutManager(this,2,GridLayoutManager.Vertical,false);
+            _recyclerView.SetLayoutManager(_layoutManager);
+
+            _gifAdapter = new GifAdapter(_gifs, this);
+            _recyclerView.SetAdapter(_gifAdapter);
+        }
+
+        private List<int> GetGifs()
+        {
+            _gifs = new List<int>();
+            _gifs.Add(Resource.Drawable.minion);
+            _gifs.Add(Resource.Drawable.minion2);
+            _gifs.Add(Resource.Drawable.minon3);
+            _gifs.Add(Resource.Drawable.minion4);
+            _gifs.Add(Resource.Drawable.minon5);
+            _gifs.Add(Resource.Drawable.minion);
+            _gifs.Add(Resource.Drawable.minion2);
+            _gifs.Add(Resource.Drawable.minon3);
+            _gifs.Add(Resource.Drawable.minion4);
+            _gifs.Add(Resource.Drawable.minon5);
+            _gifs.Add(Resource.Drawable.minion);
+            _gifs.Add(Resource.Drawable.minion2);
+            _gifs.Add(Resource.Drawable.minon3);
+            _gifs.Add(Resource.Drawable.minion4);
+            _gifs.Add(Resource.Drawable.minon5);
+
+
+
+            return _gifs;
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
